df63d998e79043fd6df03814dce321d88ef04b3a
Noworkwetrade <Noworkwetrade@gmail.com>
2026-07-08 12:22:54 -0400

    Add URL detection and logging for pocketoption

---
+++ content.js
@@ -0,0 +1,9 @@
+console.log("no work we trade loaded")
+
+const currentUrl = window.location.href
+
+if (currentUrl.includes("pocketoption")) {
+    console.log("pocket option detected")
+
+    alert("no work we trade connected")
+}
